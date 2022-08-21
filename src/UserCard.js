86a078935b70d585ab64ce05cdda4dcf6883d6b4
ELESSARX <kdephp@gmail.com>
2022-08-21 15:35:35 +0300

    add UserCard

---
+++ src/UserCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function  ({ name, text }) {
+    return <div className="card">
+        <h3>{ name }</h3>
+        <hr/>
+        <div>{ text }</div>
+    </div>
+}
